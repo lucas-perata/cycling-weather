b961e075b6d18399e470431c1f8ca0e1ef1ac182
lucas-perata <contacto.perata@gmail.com>
2022-10-06 12:09:56 -0300

    Separación en componente independiente sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,46 +1,12 @@
 import React from "react";
 import Tips from "../Tips/Tips";
+import Forecast from "../../Forecast/Forecast";
 
 const Sidebar = ({ dayForecast }) => {
   return (
     <div className="flex flex-row gap-9 justify-center">
-      <div className="flex flex-row  justify-center w-screen h-80 bg-gray-100 rounded-xl">
-        <div className="flex flex-col w-1/2 justify-around h-full">
-          <div className="ml-7">
-            <p className=" text-2xl">Próximas horas</p>
-          </div>
-          <div className=" flex justify-around text-center">
-            <div className="bg-yellow-100 border w-1/4 h-56 rounded-xl flex justify-between gap-5 p-5 flex-col">
-              <div>
-                <p className="text-xl p-1 ">
-                  {parseInt(dayForecast.list[0].dt_txt.slice(10, 16)) < 12
-                    ? dayForecast.list[0].dt_txt.slice(10, 16) + " am"
-                    : dayForecast.list[0].dt_txt.slice(10, 16) + " pm"}
-                </p>
-              </div>
-              <div className="flex justify-around">
-                <div>
-                  <p className="text-xs">temp</p>
-                  <p className="text-ls p-1">
-                    {dayForecast.list[0].main["temp"]}
-                  </p>
-                </div>
-                <div>
-                  <p className="text-xs">wind</p>
-                  <p className="text-ls p-1">
-                    {dayForecast.list[0].wind["speed"]}
-                  </p>
-                  <p className="text-ls p-1">
-                    {dayForecast.list[0].wind["deg"]}
-                  </p>
-                  <p className="text-ls p-1">
-                    {dayForecast.list[0].wind["gust"]}
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div className="flex flex-row  justify-center w-screen gap-8 border h-80 bg-gray-50 ">
+        <Forecast dayForecast={dayForecast} />
         <Tips />
       </div>
     </div>
--- src/components/TempDay/TempDay.js
+++ src/components/TempDay/TempDay.js
@@ -1,15 +1,23 @@
 import React from "react";
 
-export default function TempDay() {
+const TempDay = ({ dayForecast }) => {
   return (
-    <div className="container bg-red-600 h-80 w-1/3 rounded-xl">
-      <div className="flex flex-col justify-around  h-full">
+    <div className="container bg-white border h-80 w-1/3 rounded-xl">
+      <div className="flex flex-col justify-around h-full">
         <div className="ml-7">
-          <p className="p-5">{}</p>
-          <p className="p-5">{}</p>
+          <p className=" text-2xl">Pronóstico extendido</p>
         </div>
-        <div className="p-5  flex justify-around"></div>
+        <div className="flex gap-2 justify-around">
+          <div className=" flex justify-around text-center">
+            <div className="bg-yellow-100 border w-40 h-48 rounded-xl flex justify-center flex-row flex-wrap"></div>
+          </div>
+          <div className=" flex justify-around text-center">
+            <div className="bg-yellow-100 border w-40 h-48 rounded-xl flex justify-center flex-row flex-wrap"></div>
+          </div>
+        </div>
       </div>
     </div>
   );
-}
+};
+
+export default TempDay;
