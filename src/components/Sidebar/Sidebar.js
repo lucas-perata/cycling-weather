e659d59802d4262d11e838f7002c6d66d82a36b1
lucas-perata <contacto.perata@gmail.com>
2022-10-06 00:23:09 -0300

    Creación de componente Tips y agregado a Sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,5 +1,50 @@
 import React from "react";
+import Tips from "../Tips/Tips";
 
-export default function Sidebar() {
-  return <div className="bg-gray-300 w-screen h-96"></div>;
-}
+const Sidebar = ({ dayForecast }) => {
+  return (
+    <div className="flex flex-row gap-9 justify-center">
+      <div className="flex flex-row  justify-center w-screen h-80 bg-gray-100 rounded-xl">
+        <div className="flex flex-col w-1/2 justify-around h-full">
+          <div className="ml-7">
+            <p className=" text-2xl">Próximas horas</p>
+          </div>
+          <div className=" flex justify-around text-center">
+            <div className="bg-yellow-100 border w-1/4 h-56 rounded-xl flex justify-between gap-5 p-5 flex-col">
+              <div>
+                <p className="text-xl p-1 ">
+                  {parseInt(dayForecast.list[0].dt_txt.slice(10, 16)) < 12
+                    ? dayForecast.list[0].dt_txt.slice(10, 16) + " am"
+                    : dayForecast.list[0].dt_txt.slice(10, 16) + " pm"}
+                </p>
+              </div>
+              <div className="flex justify-around">
+                <div>
+                  <p className="text-xs">temp</p>
+                  <p className="text-ls p-1">
+                    {dayForecast.list[0].main["temp"]}
+                  </p>
+                </div>
+                <div>
+                  <p className="text-xs">wind</p>
+                  <p className="text-ls p-1">
+                    {dayForecast.list[0].wind["speed"]}
+                  </p>
+                  <p className="text-ls p-1">
+                    {dayForecast.list[0].wind["deg"]}
+                  </p>
+                  <p className="text-ls p-1">
+                    {dayForecast.list[0].wind["gust"]}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <Tips />
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ src/components/Tips/Tips.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Tips() {
+  return (
+    <>
+      <div className="container bg-white border h-80 w-1/4 rounded-xl shadow">
+        <div className="flex flex-col justify-around h-full">
+          <div className="ml-7">
+            <p className=" text-2xl">Tips para tu rodada</p>
+          </div>
+          <div className="flex gap-2 justify-around">
+            <p>Llevar protector solar</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
