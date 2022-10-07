040baaf5397d0875089bd6d9cd437588dc31f015
lucas-perata <contacto.perata@gmail.com>
2022-10-07 14:51:51 -0300

    Estilos HERO

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,20 @@
 import React from "react";
+import { GoMarkGithub } from "react-icons/go";
 
 export default function Hero() {
   return (
-    <div className="p-16 bg-green-600 h-40 mt-3">
+    <div className="p-10 flex justify-between bg-gray-100 h-40 shadow">
       <div className="w-fit">
-        <p className="text-2xl">
-          Bienvenido a Cyclist Weather, una aplicación diseñada para acompañar a
-          los ciclistas día a día
-        </p>
+        <p className="text-2xl">Bienvenido a Cyclist Weather</p> <br />
+        <p>Una aplicación diseñada para acompañar a los ciclistas día a día</p>
+      </div>
+      <div className="">
+        <a
+          href="https://github.com/lucas-perata/cycling-weather"
+          target="blank"
+        >
+          <GoMarkGithub className="mt-3" style={{ fontSize: "2.5rem" }} />
+        </a>
       </div>
     </div>
   );
