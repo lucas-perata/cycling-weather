79766131f1ba6015251b7e498c5a7bc440e8c70a
lucas-perata <contacto.perata@gmail.com>
2022-10-29 16:14:36 -0300

    Estilos y contendo page Tips

---
+++ src/components/Tips/TipsCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const TipsCard = ({ content, icon }) => {
+  return (
+    <div className="bg-slate-800 hover:bg-slate-700 w-1/4 h-52 mb-5 rounded-xl flex flex-col gap-4 text-center justify-center">
+      <i style={{ fontSize: "4rem" }} className="self-center">
+        {icon}
+      </i>
+      <p>{content}</p>
+    </div>
+  );
+};
+
+export default TipsCard;
