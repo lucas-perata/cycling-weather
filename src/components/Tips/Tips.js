d493984d48a7f784bae0a578aa360dd318fce569
lucas-perata <contacto.perata@gmail.com>
2022-10-30 15:22:24 -0300

    Tips mobile

--- src/components/Tips/Tips.js
+++ src/components/Tips/Tips.js
@@ -11,36 +11,34 @@ import { MdOutlineFastfood } from "react-icons/md";
 
 export default function Tips() {
   return (
-    <>
-      <div className=" bg-slate-900 border h-screen w-9/12">
-        <div className="flex flex-col justify-evenly h-full xs:w-screen">
-          <div className="ml-7 text-center">
-            <h2 style={{ fontSize: "3rem" }} className="text-white">
-              Tips para tu rodada
-            </h2>
-          </div>
-          <div className="flex flex-row text-white text-lg p-7 gap-2 justify-evenly flex-wrap">
-            <TipsCard
-              content={"Utilizar protector solar"}
-              icon={<BsEmojiSunglasses />}
-            />
-            <TipsCard content={"Llevar hidratación"} icon={<GiWaterFlask />} />
-            <TipsCard
-              content={"Comprobar presión de las cubiertas"}
-              icon={<GiFlatTire />}
-            />
-            <TipsCard content={"Llevar casco"} icon={<GiAstronautHelmet />} />
-            <TipsCard
-              content={"Llevar algo dulce y/o salado"}
-              icon={<MdOutlineFastfood />}
-            />
-            <TipsCard
-              content={"Equipar repuesto de cámara y parches"}
-              icon={<GiEyepatch />}
-            />
-          </div>
+    <section className="flex text-white flex-col bg-slate-900 w-3/4 xs:flex-col xs:w-full xs:pb-4">
+      <div className="flex flex-col justify-evenly h-full xs:w-screen xs:mt-10">
+        <div className="ml-7 text-center">
+          <h2 style={{ fontSize: "3rem" }} className="text-white">
+            Tips para tu rodada
+          </h2>
+        </div>
+        <div className="flex flex-row text-white text-lg p-7 gap-2 justify-evenly flex-wrap xs:flex-nowrap xs:flex-col xs:items-center">
+          <TipsCard
+            content={"Utilizar protector solar"}
+            icon={<BsEmojiSunglasses />}
+          />
+          <TipsCard content={"Llevar hidratación"} icon={<GiWaterFlask />} />
+          <TipsCard
+            content={"Comprobar presión de las cubiertas"}
+            icon={<GiFlatTire />}
+          />
+          <TipsCard content={"Llevar casco"} icon={<GiAstronautHelmet />} />
+          <TipsCard
+            content={"Llevar algo dulce y/o salado"}
+            icon={<MdOutlineFastfood />}
+          />
+          <TipsCard
+            content={"Equipar repuesto de cámara y parches"}
+            icon={<GiEyepatch />}
+          />
         </div>
       </div>
-    </>
+    </section>
   );
 }
