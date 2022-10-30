c991b40d1d27c2c956ff98e019593dbec9043b47
lucas-perata <contacto.perata@gmail.com>
2022-10-29 22:22:32 -0300

    Estilos weather main

--- src/components/Weather/NextHours.js
+++ src/components/Weather/NextHours.js
@@ -10,14 +10,14 @@ import setWindDirection from "../../hooks/setWindDirection";
 
 const NextHours = ({ dayForecast }) => {
   return (
-    <div className="flex flex-col h-min bg-white xs:w-screen xs:flex-wrap xs:overflow-scroll">
-      <h2 className="text-xl p-5">Próximas horas</h2>
+    <section className="flex flex-col h-min bg-slate-900 xs:w-screen xs:flex-wrap xs:overflow-scroll">
+      <h2 className="text-2xl p-5">Próximas horas</h2>
       <div className=" flex gap-5 p-5 justify-around text-center">
         {dayForecast.list.slice(0, 4).map((day) => {
           return (
-            <div className="bg-yellow-100 border w-1/4 h-56 rounded-xl flex justify-around gap-5 p-5 flex-col">
+            <div className="bg-slate-800 hover:bg-slate-700 shadow-xl w-1/4 h-56 rounded-xl flex items-center justify-center gap-5 p-5 flex-col">
               <div>
-                <p className="text-xl p-1 ">
+                <p className="text-xl text-slate-200 p-1">
                   {parseInt(day.dt_txt.slice(10, 16)) < 12
                     ? day.dt_txt.slice(10, 16) + " am"
                     : day.dt_txt.slice(10, 16) + " pm"}
@@ -25,48 +25,46 @@ const NextHours = ({ dayForecast }) => {
                 <p>{day.weather[0]["description"]}</p>
               </div>
               <div className="">
-                <div className="border shadow rounded-lg h-12 w-48 flex justify-center">
+                <div className="shadow-2xl rounded-xl h-12 w-48 flex justify-center">
                   <div className="flex w-full h-full justify-around">
-                    <div className="h-full">
+                    <div className="h-full flex flex-col items-center">
                       <RiTempHotLine />
                       <p className="text-ls p-1">
                         {`${day.main["temp_max"].toFixed(0)} C°`}
                       </p>
                     </div>
-                    <div>
+                    <div className="h-full flex flex-col items-center">
                       <RiTempColdLine />
                       <p className="text-ls p-1">
                         {`${day.main["temp_min"].toFixed(0)} C°`}
                       </p>
                     </div>
-                    <div>
+                    <div className="h-full flex flex-col items-center">
                       <WiHumidity />
-                      <p className="text-ls p-1">{day.main["humidity"]}</p>
+                      <p className="text-ls p-1">{`${day.main["humidity"]}%`}</p>
                     </div>
                   </div>
                 </div>
               </div>
-              <div className="">
-                <div className="border shadow h-12 w-48 flex justify-center">
-                  <div className="flex w-full h-full justify-around">
-                    <div>
-                      <RiWindyFill />
-                      <p className="text-ls p-1">
-                        {(day.wind["speed"] * 3.6).toFixed(0)}
-                      </p>
-                    </div>
-                    <div>
-                      <WiWindy />
-                      <p className="text-ls p-1">
-                        {(day.wind["gust"] * 3.6).toFixed(0)}
-                      </p>
-                    </div>
-                    <div>
-                      <RiCompass3Line />
-                      <p className="text-ls p-1">
-                        {setWindDirection(day.wind["deg"])}
-                      </p>
-                    </div>
+              <div className="shadow-xl rounded-xl h-12 w-48 flex justify-center">
+                <div className="flex w-full h-full justify-around">
+                  <div className="h-full flex flex-col items-center">
+                    <WiWindy />
+                    <p className="text-ls p-1">
+                      {(day.wind["speed"] * 3.6).toFixed(0)}
+                    </p>
+                  </div>
+                  <div className="h-full flex flex-col items-center">
+                    <RiWindyFill />
+                    <p className="text-ls p-1">
+                      {(day.wind["gust"] * 3.6).toFixed(0)}
+                    </p>
+                  </div>
+                  <div className="h-full flex flex-col items-center">
+                    <RiCompass3Line />
+                    <p className="text-ls p-1">
+                      {setWindDirection(day.wind["deg"])}
+                    </p>
                   </div>
                 </div>
               </div>
@@ -74,7 +72,7 @@ const NextHours = ({ dayForecast }) => {
           );
         })}
       </div>
-    </div>
+    </section>
   );
 };
 export default NextHours;
